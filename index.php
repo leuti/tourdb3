cb8b07f53724ae185d4267a7c982ab0cf2aca52a
LENOVO\Danny <daniel.leutwyler@gmx.ch>
2017-12-01 22:10:12 +0100

    Reading now all GPX tags

--- index.php
+++ index.php
@@ -1,35 +1,37 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<meta http-equiv="Cache-control" content="no-cache">
-  <meta http-equiv="Expires" content="-1">
+	<meta charset="utf-8">
 	<title>tourdb3</title>
 </head>
 <body>
-
   <?php
-      $verz = "C:\Users\Danny\OneDrive\Dokumente\_tourdb\gps\Strava";
-      chdir($verz);
+      // Open directory where the Strave GPX are stored
+      $verz = "C:\Users\Danny\OneDrive\Dokumente\_tourdb\gps\Strava";   // ACTION: Move to include file
+      chdir($verz);   
       $dirHandle = opendir($verz);   // Open handle
       
       echo "<h2>Directory $verz</h2>";
             
-      /* Liste alle Objktnamen */
+      // Loop through each file in directory
       while ($fileName = readdir($dirHandle))
       {
         $fullFileName = $verz . "\\" . $fileName;
         
+        // Perform following statements only for files (an not for . and ..)
         if (is_file ($fullFileName)) 
         {
-          echo "File Name: $fileName";
-          //echo "<h3>Processing $fileName</h3>";
-          //echo "<p>Hallo</p>";
-          $gpx = simplexml_load_file($fullFileName);
-          echo " Track Name: " . $gpx->trk->name . "<br>";
-          //echo "<p>Tschüss</p>";
-          fclose($fileName);
-        }
+          $gpx = simplexml_load_file($fullFileName);  // Load XML structure
+          echo " Track Name: " . $gpx->trk->name;
+          echo " --> Anzahl trk: " . count($gpx->trk) . "<br>";   // ACTION: Through error message when number of trk > 1 
+          /*foreach ($gpx->trk->trkseg->trkpt as $trkpt)
+          {
+            echo "lat: " . $trkpt["lat"] . "<br>";
+            echo "lon: " . $trkpt["lon"] . "<br>";
+            echo "Time: $trkpt->time<br><br>";
+            echo "Elevation: $trkpt->ele<br><br>";
+          }*/
+         }
       } 
       
       /* Schliesst Handle */
