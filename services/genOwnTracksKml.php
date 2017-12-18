87f30b7b11a5d2dcdd7b0e39b7f17fb54137e1a1
LENOVO\Danny <daniel.leutwyler@gmx.ch>
2017-12-18 20:38:11 +0100

    Generate Own KML - erste stabile und Output-produzierende Version

--- services/genOwnTracksKml.php
+++ services/genOwnTracksKml.php
@@ -51,7 +51,7 @@ $kml[] = '        <open>1</open>';
 
 // Select tracks meeting given WHERE clause
 
-$sql = "SELECT trkId, trkTrackName, trkRoute ";
+$sql = "SELECT trkId, trkTrackName, trkRoute, trkParticipants ";
 $sql .= "FROM tbl_tracks ";
 $sql .= $whereGenKml;
 
@@ -73,9 +73,29 @@ while($SingleTrack = mysqli_fetch_assoc($tracks))
     $kml[] = '            </Data>';
     $kml[] = '          </ExtendedData>';
     $kml[] = '          <LineString>';
-    $kml[] = '            <coordinates>';
-    $kml[] = '             8.505288351801079,47.33652743186154,0 8.505585623549026,47.33617387209225,0 8.5047957900375,47.33637906418235,0 8.50407724631536,47.33617880936397,0 8.503584511594617,47.33602144296602,0 8.503379602542626,47.33569950649844,0 8.503205283301982,47.33558417687862,0 8.503189002458406,47.33543140882657,0 8.50331842662418,47.33528629209979,0 8.503168952690865,47.3350897765492,0 8.50332411707385,47.33490844164079,0 8.503225228650079,47.33459452206319,0 8.503182848253658,47.33445998506657,0 8.503561652660359,47.33421362064634,0 8.503673354622503,47.33384378710456,0 8.503961344283187,47.33368821113609,0 8.504210722701018,47.33358696205518,0 8.504484222434984,47.3333685521373,0 8.504849604273018,47.33311331203102,0 8.504411788094075,47.33305438911223,0 8.503936441023351,47.33310375329116,0 8.502829686604496,47.33333884261586,0 8.501641250298707,47.33412338011304,0 ';
-    $kml[] = '            </coordinates>';
+
+  
+    // For each trkId loop track point and create coordinates string
+    $sqlTrkPt  = "SELECT tptLat, tptLon, tptEle ";
+    $sqlTrkPt .= "FROM tbl_trackPoints WHERE tptTrackFID = ";
+    $sqlTrkPt .= $SingleTrack["trkId"] . " ORDER BY tptNumber"; 
+    
+    $trackPoints = mysqli_query($conn, $sqlTrkPt);
+   
+    // Write track specific data to kml
+    $first = 1;
+    while($trackPoint = mysqli_fetch_assoc($trackPoints))
+    {
+        if ($first==1) 
+        {
+            $coord = $trackPoint["tptLon"] . ',' . $trackPoint["tptLat"] . ',' . $trackPoint["tptEle"];
+            $first = 0;
+        } else 
+        {
+            $coord .= ' ' . $trackPoint["tptLon"] . ',' . $trackPoint["tptLat"] . ',' . $trackPoint["tptEle"];
+        }
+    };
+    $kml[] = '            <coordinates>' . $coord . '</coordinates>';
     $kml[] = '          </LineString>';
     $kml[] = '        </Placemark>';
 
