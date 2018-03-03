1ec349fe5718c53b0f249bed412ae3a3bdc124a8
LENOVO\Danny <daniel.leutwyler@gmx.ch>
2018-03-03 07:38:37 +0100

    Clean up login.php

--- services/login.php
+++ services/login.php
@@ -8,53 +8,55 @@
 //
 // Created: 08.01.2018 - Daniel Leutwyler
 // ---------------------------------------------------------------------------------------------
+// ACTIONS
+// * Move login check to DB
 
 // -----------------------------------
 // Set variables and parameters
 include("./config.inc.php");                                        // include config file
 date_default_timezone_set('Europe/Zurich');                         // must be set when using time functions
 
-$debugLevel = 3;                                                    // 0 = off, 6 = all
+$debugLevel = 1;                                                    // 0 = off, 1 = regular, 3 = many, 6 = all
 
 // Open file for import log
-$importGpxLog = dirname(__FILE__) . "/../log/login.log";        // Assign file location
+$importGpxLog = dirname(__FILE__) . "/../log/login.log";            // Assign file location
 $logFile = @fopen($importGpxLog,"a");                               // open log file handler 
-fputs($logFile, "\r\n============================================================\r\n");    
-fputs($logFile, "login.php started: " . date("Ymd-H:i:s", time()) . "\r\n");    
+if ( $debugLevel >= 1 ) fputs($logFile, "\r\n============================================================\r\n");    
+if ( $debugLevel >= 1 ) fputs($logFile, "login.php started: " . date("Ymd-H:i:s", time()) . "\r\n");    
+if ( $debugLevel >= 1 ) fputs($logFile, "Line 26: debuglevel set to: $debugLevel\r\n");    
 
 // variables passed on by client (as formData object)
 $receivedData = json_decode ( file_get_contents('php://input'), true );
 $loginName = $receivedData["loginName"];
 $loginPasswd = $receivedData["loginPasswd"];
 
-if( $debugLevel > 1) fputs($logFile, "Line 30: Request (JSON): $loginName & $loginPasswd\r\n");    
+if ( $debugLevel >= 3 ) fputs($logFile, "Line 33: User <$loginName> has logged in.\r\n");    
 
 // Start or restart session 
 session_start();
 
-    // in case this script is started from login page
-    if(isset($loginName))
+// in case this script is started from login page
+if( isset($loginName) )
+{
+    if( $loginName == "leut" && $loginPasswd == "sugus" ||          // check if login in known --> these login must be moved to DB
+        $loginName == "admin" && $loginPasswd == "admin" )
     {
-        if( $loginName == "leut" && $loginPasswd == "sugus" ||
-            $loginName == "admin" && $loginPasswd == "admin" )
-        {
-            $_SESSION["login"] = $loginName;
-            if( $debugLevel > 3) fputs($logFile, "Line 42: $loginName\r\n");   
-        }
+        $_SESSION["login"] = $loginName;                            // set session var
     }
+}
 
-    // check if within sessioin
-    if(isset($_SESSION["login"]))
-    {    
-        $returnObject['sessionid'] = session_id();                   // add field coordinates to track object
-        $returnObject['loginstatus'] = "OK";
-                                // echo track object to client
-    } else {
-        $returnObject['sessionid'] = "";                   // add field coordinates to track object
-        $returnObject['loginstatus'] = "ERROR";
-    }
-    echo json_encode($returnObject); 
-    if( $debugLevel > 3) {
-        fputs($logFile, "Line 58: sessionid: " . $returnObject['sessionid'] . " || " . $returnObject['loginstatus'] . "\r\n");
-    }
+// check if within session
+if( isset($_SESSION["login"]) )
+{    
+    $returnObject['sessionid'] = session_id();                      // add session id to return object
+    $returnObject['loginstatus'] = "OK";                            // set login status to OK
+} else {
+    $returnObject['sessionid'] = "";                                // set session id to EMPTY 
+    $returnObject['loginstatus'] = "ERROR";                         // Return error
+}
+echo json_encode($returnObject);                                    // encode return object to JSON
+if( $debugLevel >= 1 ) {
+    fputs($logFile, "Line 59: Login script completed --> sessionid: " . $returnObject['sessionid'] . " | login: " 
+    . $loginName . " | loginstatus: " . $returnObject['loginstatus'] . "\r\n");
+}
 ?>
