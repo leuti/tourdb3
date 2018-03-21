ccfced962f836163c7826f20525eec5a4131f6b6
LENOVO\Danny <daniel.leutwyler@gmx.ch>
2018-03-21 23:44:00 +0100

    Display objects now drawing map for all object types (theoretically) - still very buggy

---
+++ services/no_kml.php
@@ -0,0 +1,23 @@
+<?php
+// ---------------------------------------------------------------------------------------------
+// This script returns an empty OK message. This is because I currently have no solution for the 
+// $.ajax().when for multiple calls (calls only required when genKml = true)
+
+// Created: 21.03.2018 - Daniel Leutwyler
+// ---------------------------------------------------------------------------------------------
+//
+// Tasks
+// * 
+
+// Create return object
+$returnObject['status'] = 'OK';                                             // add status field (OK) to trackobj
+$returnObject['message'] = 'This php returns always an empty OK message';   // add empty error message to trackobj
+$returnObject['coordTop'] = 74000;
+$returnObject['coordBottom'] = 297000;
+$returnObject['coordLeft'] = 840000;
+$returnObject['coordRight'] = 110000;
+echo json_encode($returnObject);                                            // echo JSON object to client
+exit;
+
+?>
+
