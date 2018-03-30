f98aa563f2b6e315be467119745c70e6d1add7c9
LENOVO\Danny <daniel.leutwyler@gmx.ch>
2018-03-30 21:56:41 +0200

    Display of peaks_cant set to != 0; peaks_cant have now canton abbr. in DB

--- db/tourdb3_1.2/tableChanges.sql
+++ db/tourdb3_1.2/tableChanges.sql
@@ -15,6 +15,7 @@ ADD `segCoordLeft` INT(11) NOT NULL COMMENT 'X coord of western most point of se
 ADD `segCoordRight` INT(11) NOT NULL COMMENT 'X coord of easter most point of segments' AFTER `segCoordLeft`;
 
 ALTER TABLE `tbl_waypoints` ADD `waypToOfCant` TINYINT(1) NOT NULL AFTER `waypUIAA4000`;
+ALTER TABLE `tbl_waypoints` CHANGE `waypToOfCant` `waypToOfCant` VARCHAR(2) NOT NULL;
 
 -- Cleanup coordinates of waypoints
 UPDATE `tourdb2_prod`.`tbl_waypoints` 
